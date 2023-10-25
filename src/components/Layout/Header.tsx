78323e761dbcbf45bdd5a0e172a953da6f8d8373
raihan2bd <raihan2bd.official@gmail.com>
2023-10-25 20:02:25 +0600

    Add basic header with css

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,7 +1,19 @@
 "use client"
+import Link from "next/link";
 
 const Header = () => {
-  return<h1>Vidverse</h1>
+  return (
+    <header className="flex flex-row justify-between gap-1 p-4 md:px-6 bg-violet-900 text-white items-center">
+      <div className=""><Link href="/">Vidverse</Link></div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
 export default Header;
