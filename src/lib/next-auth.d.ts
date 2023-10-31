48897e6a5ddf60a6c46930c759e1518d71892dd2
raihan2bd <raihan2bd.official@gmail.com>
2023-10-31 20:53:27 +0600

    Add custom types for next-auth

---
+++ src/lib/next-auth.d.ts
@@ -0,0 +1,29 @@
+import NextAuth from 'next-auth';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: number;
+      user_name: string;
+      user_role: string;
+    };
+    token: string;
+    expires_at: number;
+    error?: "RefreshAccessTokenError"
+  }
+}
+
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    user: {
+      id: number;
+      user_name: string;
+      user_role: string;
+    };
+    token: string;
+    expires_at: number;
+    error?: "RefreshAccessTokenError"
+  }
+}
