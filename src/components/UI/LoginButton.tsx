367ca3f736ed45a210072f6bf63449f325cf60ee
raihan2bd <raihan2bd.official@gmail.com>
2023-10-31 22:15:19 +0600

    Add signout functionality

--- src/components/UI/LoginButton.tsx
+++ src/components/UI/LoginButton.tsx
@@ -3,13 +3,13 @@ import { useSession } from "next-auth/react";
 import React, { useEffect } from "react";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
+import Button from "./Button";
 
 const LoginButton = () => {
   const { data: session } = useSession();
 
   useEffect(() => {
     if (session?.error === "RefreshAccessTokenError") {
-      console.log("I'm called")
       signOut()
     };
   }, [session]);
@@ -18,12 +18,13 @@ const LoginButton = () => {
     return (
       <div className="flex gap-4 ml-auto">
         <p className="text-sky-600">{session.user.user_name}</p>
-        <Link
-          href={"/api/auth/signout"}
+        <Button type="button" onClick={() => {
+          signOut()
+        }}
           className="flex gap-4 ml-auto text-red-600"
         >
           Sign Out
-        </Link>
+        </Button>
       </div>
     );
 
