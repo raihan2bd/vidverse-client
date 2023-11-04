7275da6e5a7b263d7e70d34881c1002bdb25e1e3
raihan2bd <raihan2bd.official@gmail.com>
2023-11-04 21:01:46 +0600

    Add custom UI layout component

---
+++ src/components/Layout/UILayout.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { ReactNode, useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+interface PropTypes {
+  children: ReactNode;
+}
+
+const UILayout = ({children}: PropTypes) => {
+  const [showSideBar, setShowSideBar] = useState(false)
+
+  const showSideBarHandler = () => {
+    setShowSideBar(true)
+  }
+
+  const hideSideBarHandler = () => {
+    setShowSideBar(false)
+  }
+  
+  return (
+    <>
+    <Header onSetShowSideBar={setShowSideBar} showSideBar={showSideBar} />
+    <main>
+      {children}
+    </main>
+    <Footer />
+    </>
+  );
+}
+
+export default UILayout;
