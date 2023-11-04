b7bca59578e74f368870523f8f992a89e025d6c6
raihan2bd <raihan2bd.official@gmail.com>
2023-11-04 21:02:32 +0600

    Add custom layout into the main layout and added hambarger menu inside the header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,27 +1,41 @@
-"use client"
+"use client";
 
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import Link from "next/link";
 import LoginButton from "../UI/LoginButton";
 import { BiMenu } from "react-icons/bi";
 
-const Header = () => {
-  const [showNav, setShowNav] = useState(false);
-  const handleToggle = () => setShowNav(!showNav);
-  const handleClose = () => setShowNav(false);
+interface PropsTypes {
+  showSideBar: boolean;
+  onSetShowSideBar: Dispatch<SetStateAction<boolean>>;
+}
+
+const Header = ({ onSetShowSideBar, showSideBar }: PropsTypes) => {
+  const showSidebarHandler = () => onSetShowSideBar(true);
+  const hideSidebarHandler = () => onSetShowSideBar(false);
+  const toggleSidebarHandler = () => onSetShowSideBar((prevState) => !prevState);
+
+  const hamburgerMenuClass = showSideBar? "text-2xl text-red-400 font-black": "text-2xl text-white font-black";
 
   return (
     <header className="flex flex-row justify-between gap-1 p-4 md:px-6 bg-violet-900 text-white items-center fixed w-[100%] h-[5rem] z-50">
-      <div><BiMenu /><Link href="/" className="text-2xl text-yellow-400 font-black">Vidverse</Link></div>
+      <div className="flex gap-2 items-center">
+        <span className={hamburgerMenuClass} onClick={toggleSidebarHandler}>
+          <BiMenu />
+        </span>
+        <Link href="/" className="text-2xl text-yellow-400 font-black">
+          Vidverse
+        </Link>
+      </div>
       <nav>
         <ul>
           <li>
-          <LoginButton />
+            <LoginButton />
           </li>
         </ul>
       </nav>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
