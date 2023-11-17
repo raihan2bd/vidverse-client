2858f34728c9b7002cb60b59e12d5dc1d5af1ae9
raihan2bd <raihan2bd.official@gmail.com>
2023-11-17 21:13:02 +0600

    Update the sidebar according to the props

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -3,11 +3,11 @@
 import Button from "../UI/Button";
 
 interface PropTypes {
-  showSideBar: boolean
+  sidebarClasses: string
 }
 
-const Sidebar = ({ showSideBar }: PropTypes) => {
-  const sidebarClasses = showSideBar ? "block md:hidden fixed pt-[5rem] left-0 bottom-0 z-10 h-screen w-[100%] bg-white md:w-1/5 md:min-w-[280px] overflow-y-scroll thin-scrollbar text-black": "hidden md:block fixed pt-[5rem] left-0 bottom-0 z-10 h-screen w-[100%] bg-white md:w-[23%] overflow-y-scroll thin-scrollbar text-black"
+const Sidebar = ({ sidebarClasses }: PropTypes) => {
+
   return (
     <aside className={sidebarClasses}>
       <h1 className="text-black text-4xl font-bold">Sidebar</h1>
