bccd98b12d5f4340adbedd4f633fde5d964fee66
raihan2bd <raihan2bd.official@gmail.com>
2023-11-18 22:08:03 +0600

    Add custom helper function to get all related videos

---
+++ src/lib/getRelatedVideos.ts
@@ -0,0 +1,16 @@
+export default async (id: number,) => {
+  const apiUrl = process.env.NEXT_API_URL;
+  const response = await fetch(`${apiUrl}/api/v1/related_videos/${id}`, { cache: 'no-store' });
+  if(!response.ok) {
+    if(response.status === 401) {
+      // Logout the current user
+    }
+    if(response.status === 404) {
+       throw new Error('No videos found');
+    } else {
+      throw new Error('Something went wrong. please try again later');
+    }
+  }
+  const result = await response.json();
+  return result;
+};
