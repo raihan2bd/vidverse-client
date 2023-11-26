b42cd424fc7f9eccb0f83012f2c1d4115e308fe1
raihan2bd <raihan2bd.official@gmail.com>
2023-11-26 14:28:19 +0600

    Update upload video page with necessary form input including css styles

--- src/app/dashboard/upload-video/page.tsx
+++ src/app/dashboard/upload-video/page.tsx
@@ -4,16 +4,21 @@ import React, { useState, useEffect } from "react";
 import useInput from "@/hooks/useInput";
 import { validateInput } from "@/utils/validator";
 import { useSearchParams, useRouter } from "next/navigation";
+import ErrorModal from "@/components/UI/ErrorModal";
+import Spinner from "@/components/UI/Spinner";
+import Input from "@/components/UI/input";
+import Button from "@/components/UI/Button";
 
 const API_URL = process.env.NEXT_API_URL;
 
 const UploadOrEditVideoPage = () => {
-  const searchParams = useSearchParams()
-  const router = useRouter()
-  const videoId = searchParams.get('edit') !== '' ? searchParams.get('edit') : null
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const videoId =
+    searchParams.get("edit") !== "" ? searchParams.get("edit") : null;
   const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
-
+  const [hasError, setError] = useState<string | null>(null);
+  const [formError, setFormError] = useState<string | null>(null);
   const {
     value: title,
     errorMsg: titleError,
@@ -53,8 +58,6 @@ const UploadOrEditVideoPage = () => {
     setSelectedChannel(e.target.value);
   };
   const [loadingChannels, setLoadingChannels] = useState<boolean>(false);
-  const [errorChannels, setErrorChannels] = useState<string | null>(null);
-
   // fetch video data if videoId is present
   useEffect(() => {
     if (videoId) {
@@ -73,10 +76,14 @@ const UploadOrEditVideoPage = () => {
           const status = error.response.status || 500;
           switch (status) {
             case 401:
-              router.push(`/login?callback=/dashboard/upload-video${videoId  && videoId !== '' ? `?edit=${videoId}` : ''}`)
+              router.push(
+                `/login?callback=/dashboard/upload-video${
+                  videoId && videoId !== "" ? `?edit=${videoId}` : ""
+                }`
+              );
               break;
             case 403:
-             router.push(`/access-denied`)
+              router.push(`/access-denied`);
             case 404:
               setError("Video not found");
               break;
@@ -86,10 +93,10 @@ const UploadOrEditVideoPage = () => {
           }
           setLoading(false);
         }
-      }
+      };
       fetchVideo();
     }
-  }, [videoId])
+  }, [videoId]);
 
   // fetch channels
   useEffect(() => {
@@ -99,32 +106,40 @@ const UploadOrEditVideoPage = () => {
         const response = await fetch(`${API_URL}/v1/api/channels`);
         const data = await response.json();
         if (data.channels.length > 0) {
-          setErrorChannels("No channels found");
+          setError("No channels found");
         } else {
           setSelectedChannel(data.channel.id);
         }
         setLoadingChannels(false);
       } catch (error: any) {
-        const status = error.response.status || 500;
-          switch (status) {
-            case 401:
-              router.push(`/login?callback=/dashboard/upload-video${videoId  && videoId !== '' ? `?edit=${videoId}` : ''}`)
-              break;
-            case 403:
-             router.push(`/access-denied`)
-            case 404:
-              setError("Video not found");
-              break;
-            default:
-              setError("Something went wrong");
-              break;
-          }
-          setLoading(false);
+        console.log(error);
+        const status =
+          error && error.response && error.response.status
+            ? error.response.status
+            : 500;
+        switch (status) {
+          case 401:
+            router.push(
+              `/login?callback=/dashboard/upload-video${
+                videoId && videoId !== "" ? `?edit=${videoId}` : ""
+              }`
+            );
+            break;
+          case 403:
+            router.push(`/access-denied`);
+          case 404:
+            setError("Video not found");
+            break;
+          default:
+            setError("Something went wrong");
+            break;
+        }
+        setLoading(false);
       }
     };
 
     fetchChannels();
-  }, [])
+  }, []);
 
   const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -140,11 +155,87 @@ const UploadOrEditVideoPage = () => {
     }
   };
 
+  if (loading) {
+    return (
+      <div className="bg-black/25 w-[100vw] overflow-hidden max-w-[100%] h-screen flex justify-center items-center">
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (hasError) {
+    return (
+      <ErrorModal errMsg={hasError} tryAgainHandler={() => setError(null)} />
+    );
+  }
+
   return (
-    <div>
-      <h1>Upload Video</h1>
+    <div className="w-full flex justify-center p-4 bg-black/30">
+      <form
+        onSubmit={submitHandler}
+        className="w-[768px] max-w-[100%] p-4 md:p-6 min-h-screen bg-white rounded-2xl flex flex-col gap-2"
+      >
+        <h1 className="py-1 text-2xl font-bold text-violet-700 block w-fit border-0 border-b-[3px] mx-auto border-bg-black/30">
+          {videoId ? "EDIT VIDEO" : "UPLOAD VIDEO"}
+        </h1>
+
+        <Input
+          name="title"
+          label="Title"
+          type="text"
+          value={title}
+          placeholder="Enter your video title"
+          onChange={titleChangeHandler}
+          onBlur={titleBlurHandler}
+          inputError={titleIsTouched ? titleError : null}
+        />
+
+        <Input type="file" name="video" label="Video" />
+
+        <div className="flex flex-col gap-2">
+          <label className="text-sm text-black/90" htmlFor="description">
+            Description:
+          </label>
+          <textarea
+            className="border border-black/30 rounded-sm p-2 w-full h-32"
+            name="description"
+            id="description"
+            value={description}
+            onChange={descriptionChangeHandler}
+            onBlur={descriptionBlurHandler}
+            placeholder="Enter your Video description"
+          ></textarea>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <label className="text-sm text-black/90" htmlFor="channel">
+            Channel:
+          </label>
+          <select
+            name="channel"
+            id="channel"
+            className="border border-black/30 rounded-sm p-2 w-full"
+            onChange={changeChannelHandler}
+            value={selectedChannel}
+          >
+            {loadingChannels ? (
+              <option>Loading...</option>
+            ) : (
+              channels.map((channel: any) => (
+                <option key={channel.id} value={channel.id}>
+                  {channel.name}
+                </option>
+              ))
+            )}
+          </select>
+        </div>
+
+        <Input type="file" name="thumbnail" label="Thumbnail" />
+
+        <Button type="submit" btnClass="w-full py-3 mt-4">Upload Video</Button>
+      </form>
     </div>
   );
-}
+};
 
 export default UploadOrEditVideoPage;
