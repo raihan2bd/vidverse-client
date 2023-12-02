9bc00c37b2e3d3b509c01d6a01e721e2eb0d4b48
raihan2bd <raihan2bd.official@gmail.com>
2023-12-02 22:27:07 +0600

    Add get single channel helper function

---
+++ src/lib/getSingleChannel.ts
@@ -0,0 +1,16 @@
+export default async (chanID: number) => {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  const response = await fetch(`${API_URL}/api/v1/channels/${chanID}`, { cache: 'no-store' });
+  if(!response.ok) {
+    if(response.status === 401) {
+      // Logout the current user
+    }
+    if(response.status === 404) {
+       throw new Error('No channels found');
+    } else {
+      throw new Error('Something went wrong. please try again later');
+    }
+  }
+  const result = await response.json();
+  return result.channel;
+}
