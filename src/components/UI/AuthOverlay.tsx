136fd2e3c5d5d74955019f49ff30e6176ea3eb66
raihan2bd <raihan2bd.official@gmail.com>
2024-01-20 16:34:02 +0600

    Refactor AuthOverlay component

--- src/components/UI/AuthOverlay.tsx
+++ src/components/UI/AuthOverlay.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { ReactNode } from 'react';
-import classes from './AuthOverlay.module.css'
+import { ReactNode } from "react";
+import classes from "./AuthOverlay.module.css";
 
-const AuthOverlay = ({children}: {children: ReactNode}) => {
+const AuthOverlay = ({ children }: { children: ReactNode }) => {
   return (
-  <div className={classes.container}>
-    {children}
-  </div>)
-}
+    <div className={classes.container}>
+      <div className="flex flex-col min-h-screen w-[767px] max-w-[100%] relative z-[1] my-4">
+        {children}
+      </div>
+    </div>
+  );
+};
 
 export default AuthOverlay;
