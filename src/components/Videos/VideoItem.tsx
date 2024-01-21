5d7f5fc23b2c394cc8881079f87990afcfdea153
NurkaAmre <nurkerey30@gmail.com>
2024-01-21 14:15:38 +0600

    Refactor video card design

--- src/components/Videos/VideoItem.tsx
+++ src/components/Videos/VideoItem.tsx
@@ -10,7 +10,7 @@ interface VideoItemProps {
 
 const VideoItem = ({ video }: VideoItemProps) => {
   return (
-    <li className="w-[100%] max-w-[100%] shrink-1 rounded bg-white p-2 shadow-md flex flex-col justify-between gap-2 overflow-hidden">
+    <li className="w-[100%] max-w-[100%] shrink-1 bg-gradient-to-t from-[#BEB8E7] to-purple-white  p-2 flex flex-col justify-between overflow-hidden">
       <Link href={`/videos/${video.id}`} className="block">
         <div className="relative w-full h-[240px] sm:h-[200px]">
           <Image
@@ -22,21 +22,21 @@ const VideoItem = ({ video }: VideoItemProps) => {
             priority={true}
           />
         </div>
-        <h4 className="text-sky-700 text-sm font-bold p-2">
+        <h4 className="text-custom-blue-400 text-sm font-bold pt-2">
           {video.title.length > 70
             ? `${video.title.substring(0, 70)}...`
             : video.title}
         </h4>
       </Link>
-      <div className="text-xs text-slate-700 flex justify-between items-center p-2">
+      <div className="text-xs text-slate-700 flex gap-2 pt-1 items-center">
         <Link
           className="flex justify-between gap-3 items-center"
           href={`/channels/${video.channel_id}`}
         >
-          <div className="relative w-[24px] h-[24px] rounded-full">
+          <div className="relative w-[30px] text-custom-blue-600 font-semibold h-[30px] rounded-full">
             <Image
               src={
-                video.channel_logo && video.channel_logo !== ""
+                video.channel_logo && video.channel_logo !== ''
                   ? video.channel_logo.toString()
                   : noThumb.src
               }
@@ -45,10 +45,19 @@ const VideoItem = ({ video }: VideoItemProps) => {
               className="rounded-full"
               sizes="24px"
             />
-          </div>{" "}
-          {video.channel_title}
+          </div>{' '}
         </Link>
-        <span className="text-gray-500">{`${video.views} views`}</span>
+        <div className="flex flex-col">
+          <div className="flex font-bold justify-between">
+            <span className="">{`${video.views}K views`}</span>
+            <span>{video.channel_title}</span>
+          </div>
+          <div className="text-xs text-custom-blue-500">
+            <p>
+              Container house <span> ~ 2 hours ago</span>
+            </p>
+          </div>
+        </div>
       </div>
     </li>
   );
