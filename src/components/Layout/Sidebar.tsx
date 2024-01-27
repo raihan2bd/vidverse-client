be084da9f97b34397ecb9b925c520eb784f16e13
raihan2bd <raihan2bd.official@gmail.com>
2024-01-27 13:27:03 +0600

    Update Sidebar component with new icons and state

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -5,15 +5,16 @@ import { ImHome, ImInfo } from "react-icons/im";
 import { FaFilm } from "react-icons/fa6";
 import { FaHeadphones, FaFolderPlus } from "react-icons/fa";
 import { FiLogIn } from "react-icons/fi";
-import { FaPenToSquare, FaThumbsUp } from "react-icons/fa6";
+import { FaPenToSquare, FaThumbsUp, FaClockRotateLeft, FaClock, FaAngleDown, FaAngleUp } from "react-icons/fa6";
 import { MdCancel } from "react-icons/md";
 import { SiYoutubestudio } from "react-icons/si";
-import { IoMdMail } from "react-icons/io";
+import { IoMdMail, IoIosSettings } from "react-icons/io";
 
-import { useMemo } from "react";
+
+import { useMemo, useState } from "react";
 import { signOut } from "next-auth/react";
 
-import shortsIcon from '../../../public/images/shorts-icon.svg'
+import shortsIcon from "../../../public/images/shorts-icon.svg";
 import Image from "next/image";
 
 interface PropTypes {
@@ -29,6 +30,7 @@ const Sidebar = ({
   user_id,
   user_role,
 }: PropTypes) => {
+  const [showMore, setShowMore] = useState(false)
   const year = new Date().getFullYear().toString();
 
   const navLinksCls =
@@ -37,93 +39,93 @@ const Sidebar = ({
   const authLinksCls =
     "w-full flex gap-2 px-4 py-3 font-bold bg-slate-800 text-orange-300 items-center hover:text-orange-700 hover:bg-white/10 md:hover:bg-black/20 hover:ps-8 transition-all duration-200 ease-in-out active:bg-white/10 md:active:bg-black/20";
 
-  const authBtns = useMemo(() => {
-    if (user_id) {
-      return (
-        <li className="mt-16">
-          <button
-            className={authLinksCls}
-            onClick={() => {
-              signOut();
-              onHideSidebar();
-            }}
-          >
-            <span>
-              <FiLogIn />
-            </span>
-            Logout
-          </button>
-        </li>
-      );
-    } else {
-      return (
-        <>
-          <li className="mt-16" onClick={onHideSidebar}>
-            <Link href="/login" className={authLinksCls}>
-              <span>
-                <FiLogIn />
-              </span>
-              Login
-            </Link>
-          </li>
-          <li onClick={onHideSidebar}>
-            <Link href="/register" className={authLinksCls}>
-              <span>
-                <FaPenToSquare />
-              </span>
-              Register
-            </Link>
-          </li>
-        </>
-      );
-    }
-  }, [user_id, onHideSidebar]);
+  // const authBtns = useMemo(() => {
+  //   if (user_id) {
+  //     return (
+  //       <li className="mt-16">
+  //         <button
+  //           className={authLinksCls}
+  //           onClick={() => {
+  //             signOut();
+  //             onHideSidebar();
+  //           }}
+  //         >
+  //           <span>
+  //             <FiLogIn />
+  //           </span>
+  //           Logout
+  //         </button>
+  //       </li>
+  //     );
+  //   } else {
+  //     return (
+  //       <>
+  //         <li className="mt-16" onClick={onHideSidebar}>
+  //           <Link href="/login" className={authLinksCls}>
+  //             <span>
+  //               <FiLogIn />
+  //             </span>
+  //             Login
+  //           </Link>
+  //         </li>
+  //         <li onClick={onHideSidebar}>
+  //           <Link href="/register" className={authLinksCls}>
+  //             <span>
+  //               <FaPenToSquare />
+  //             </span>
+  //             Register
+  //           </Link>
+  //         </li>
+  //       </>
+  //     );
+  //   }
+  // }, [user_id, onHideSidebar]);
 
-  const roleBasedLinks = useMemo(() => {
-    if (user_role === "admin" || (user_role === "author" && user_id)) {
-      return (
-        <>
-          <li onClick={onHideSidebar}>
-            <Link href="/dashboard/upload-video" className={navLinksCls}>
-              <span>
-                <FaFolderPlus />
-              </span>
-              New Video
-            </Link>
-          </li>
-          <li onClick={onHideSidebar}>
-            <Link href="/channels" className={navLinksCls}>
-              <span>
-                <SiYoutubestudio />
-              </span>
-              Channels
-            </Link>
-          </li>
-        </>
-      );
-    } else {
-      return (
-        <>
-          <li onClick={onHideSidebar}>
-            <Link href="/liked-videos" className={navLinksCls}>
-              <span>
-                <FaThumbsUp />
-              </span>
-              Liked Videos
-            </Link>
-          </li>
-          <li onClick={onHideSidebar}>
-            <Link href="/contact-us?req_for=author" className={navLinksCls}>
-              <span>
-                <IoMdMail />
-              </span>
-              Request for Author
-            </Link>
-          </li>
-        </>
-      );
-    }
-  }, [user_role, user_id, onHideSidebar]);
+  // const roleBasedLinks = useMemo(() => {
+  //   if (user_role === "admin" || (user_role === "author" && user_id)) {
+  //     return (
+  //       <>
+  //         <li onClick={onHideSidebar}>
+  //           <Link href="/dashboard/upload-video" className={navLinksCls}>
+  //             <span>
+  //               <FaFolderPlus />
+  //             </span>
+  //             New Video
+  //           </Link>
+  //         </li>
+  //         <li onClick={onHideSidebar}>
+  //           <Link href="/channels" className={navLinksCls}>
+  //             <span>
+  //               <SiYoutubestudio />
+  //             </span>
+  //             Channels
+  //           </Link>
+  //         </li>
+  //       </>
+  //     );
+  //   } else {
+  //     return (
+  //       <>
+  //         <li onClick={onHideSidebar}>
+  //           <Link href="/liked-videos" className={navLinksCls}>
+  //             <span>
+  //               <FaThumbsUp />
+  //             </span>
+  //             Liked Videos
+  //           </Link>
+  //         </li>
+  //         <li onClick={onHideSidebar}>
+  //           <Link href="/contact-us?req_for=author" className={navLinksCls}>
+  //             <span>
+  //               <IoMdMail />
+  //             </span>
+  //             Request for Author
+  //           </Link>
+  //         </li>
+  //       </>
+  //     );
+  //   }
+  // }, [user_role, user_id, onHideSidebar]);
 
   return (
     <aside
@@ -137,7 +139,7 @@ const Sidebar = ({
       </span>
 
       <nav className="pt-20 md:pt-2 h-full w-[80%] md:w-full max-w-[100%] bg-custom-violet-500/60 absolute z-[2] overflow-y-auto">
-        <ul className="list-none flex flex-col gap-[2px] text-white p-4">
+        <ul className="list-none flex flex-col gap-[2px] text-white px-4">
           <li onClick={onHideSidebar}>
             <Link href="/" className={navLinksCls}>
               <span>
@@ -162,28 +164,63 @@ const Sidebar = ({
               Videos
             </Link>
           </li>
-          <div className="p-3"></div>
-          {roleBasedLinks}
+          <div className="py-2"></div>
           <li onClick={onHideSidebar}>
-            <Link href="/contact-us" className={navLinksCls}>
+            <Link href="/history" className={navLinksCls}>
               <span>
-                <FaHeadphones />
+              <FaClockRotateLeft />
               </span>
-              Contact Us
+              History
             </Link>
           </li>
           <li onClick={onHideSidebar}>
-            <Link href="/about" className={navLinksCls}>
+            <Link href="/watch-later" className={navLinksCls}>
               <span>
-                <ImInfo />
+              <FaClock />
               </span>
-              About
+              Watch Later
             </Link>
           </li>
-          {authBtns}
+          <li onClick={onHideSidebar}>
+            <Link href="/liked-videos" className={navLinksCls}>
+              <span>
+                <FaThumbsUp />
+              </span>
+              Liked Videos
+            </Link>
+          </li>
+          <li onClick={onHideSidebar}>
+            <Link href="/channels" className={navLinksCls}>
+              <span>
+                <SiYoutubestudio />
+              </span>
+              Your Channels
+            </Link>
+          </li>
+          <li onClick={onHideSidebar}>
+            <button className={navLinksCls} onClick={() => setShowMore((prev) => !prev)} style={{width: '100%'}}>
+              <span>
+                {showMore ? <FaAngleUp /> : <FaAngleDown />}
+              </span>
+              Show more
+            </button>
+          </li>
+          {showMore && <p className="bg-custom-purple-400 p-2 rounded text-xs">This feature is under construction</p>}
+
+          <li className="mt-8 flex flex-row gap-4 justify-center items-center text-white text-xl" onClick={onHideSidebar}>
+            <Link href="/about">
+              <ImInfo />
+            </Link>
+            <Link href="/contact-us">
+              <FaHeadphones />
+            </Link>
+            <Link href="/">
+              <IoIosSettings />
+            </Link>
+          </li>
         </ul>
       </nav>
-      <footer className="fixed z-[2] bottom-0  h-[3.2rem]  w-[80%] md:w-full max-w-full overflow-hidden text-sm text-white border-0 border-t p-2 text-center">
+      <footer className="fixed z-[2] bottom-0 w-[80%] md:w-full max-w-full overflow-hidden text-sm text-white border-0 border-t p-2 text-center">
         Copyright &copy; {String(year)} Vidverse&#174;. All rights reserved.
       </footer>
     </aside>
