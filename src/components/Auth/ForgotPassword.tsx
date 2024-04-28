e1fbb581dd66c4ba57acad28992230170770a9e0
raihan2bd <raihan2bd.official@gmail.com>
2024-04-28 23:49:06 +0600

    Update password reset

--- src/components/Auth/ForgotPassword.tsx
+++ src/components/Auth/ForgotPassword.tsx
@@ -1,23 +1,52 @@
 "use client";
 
 import axios from "axios";
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
+import Button from "../UI/Button";
+import { useGlobalState } from "@/context/store";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL + '/api/v1';
+const API_URL = process.env.NEXT_PUBLIC_API_URL + '/api/v1/auth';
 
 const ForgotPassword = () => {
+  const {
+    setError,
+    setSuccess,
+  } = useGlobalState();
+
+  const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [isEmailSent, setIsEmailSent] = useState(false);
   const [isVerified, setIsVerified] = useState(false);
   const [timer, setTimer] = useState(0);
+  const [userId, setUserId] = useState(null);
+  const [code, setCode] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const isPasswordMatch = password === confirmPassword;
+
+  useEffect(() => {
+    if(isVerified && timer === 0) {
+      if(!isPasswordMatch) {
+        if (password.length > 0 && confirmPassword.length > 0) {
+
+          setError("Password does not match");
+        }
+      }
+    }
+  }
+  , [isVerified, timer, isPasswordMatch]);
+
 
   // send email
-  const onSendEmail = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const onSendEmail = async () => {
+
     if (timer > 0) return;
     try {
+      setIsLoading(true);
       const response = await axios.post(`${API_URL}/request-password-reset`, { email });
       if (response.status === 201) {
+        setSuccess("An Email sent to your inbox. Please check your inbox");
         setIsEmailSent(true);
         setTimer(25);
         const interval = setInterval(() => {
@@ -26,15 +55,36 @@ const ForgotPassword = () => {
         setTimeout(() => {
           clearInterval(interval);
         }, 25000);
+
       }
-    } catch (error) {
-      console.error(error);
+    
+    } catch (error :any) {
+      const msg = error.response.data.error || error.message || "Something went wrong. Please try again later"
+      setError(msg);
+    } finally {
+      setIsLoading(false);
     }
   };
 
+  const onVerifyCode = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.post(`${API_URL}/verify-password-reset`, { email, otp: code });
+      if (response.status === 200) {
+        setIsVerified(true);
+        setUserId(response.data.user_id);
+      }
+    } catch (error :any) {
+      const msg = error.response.data.error || error.message || "Something went wrong. Please try again later"
+      setError(msg);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg">
+    <div className="flex flex-col items-center justify-center h-[calc(100vh-5rem)] bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-lg max-w-[580px] w-[96%]">
         <h1 className="text-2xl font-bold text-center mb-4">Forgot Password</h1>
         {!isEmailSent && !isVerified && (
           <p className="text-gray-500 text-center mb-8">
@@ -45,24 +95,68 @@ const ForgotPassword = () => {
         {isEmailSent && !isVerified ? (
           <div className="text-center mb-4">
             <h2 className="text-lg font-bold mb-2">Email Sent</h2>
-            <p className="text-gray-500">
+            <p className="text-gray-500 overflow-hidden">
               We have sent an email to <strong>{email}</strong> with a one-time
               secret code. Please check your inbox and enter the code below.
             </p>
             <div className="mt-4">
               <input
                 type="text"
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
                 className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               />
             </div>
 
-            <button className="mt-4 w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              Verify Code
-            </button>
+            <div className="w-fit mx-auto mt-4"><Button disabled={isLoading} onClick={onVerifyCode} btnClass="w-full mx-auto">
+              {isLoading ? "Sending":  "Verify Code"}
+            </Button></div>
+            
           </div>
-        ) : (
+        ) : isEmailSent && isVerified ?(
           <form
-            onSubmit={onSendEmail}
+            onSubmit={(e) => {
+              e.preventDefault();
+              onSendEmail
+              }
+            }
+          >
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <input type="password" id="password" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Confirm Password
+              </label>
+              <input type="password" id="password" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+            </div>
+
+            <div className="w-fit mx-auto">
+            <Button
+              type="submit"
+              disabled={isLoading || !isPasswordMatch}
+            >
+              {isLoading ? "Sending..." : "Reset Password"}
+            </Button>
+            </div>
+          </form>
+        ): (
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              onSendEmail();
+              }
+            }
           >
             <div className="mb-4">
               <label
@@ -79,22 +173,25 @@ const ForgotPassword = () => {
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
-            <button
+
+            <div className="w-fit mx-auto">
+            <Button
               type="submit"
-              className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              disabled={isLoading || timer > 0}
             >
-              Send Email
-            </button>
+              {isLoading ? "Sending..." : "Request Reset Password"}
+            </Button>
+            </div>
           </form>
         )}
-        {isEmailSent && (
+        {isEmailSent && !isVerified && (
           <div className="text-center mt-4">
             <p className="text-gray-500">
               Don't get any email?{" "}
               {timer > 0 ? (
                 "Retry in " + timer + "soconds"
               ) : (
-                <button className="text-indigo-600">Resend</button>
+                <button className="text-indigo-600" onClick={onSendEmail}>Resend</button>
               )}
             </p>
           </div>
